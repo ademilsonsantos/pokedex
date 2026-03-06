d437ec44c1d0d9dd6ed297e735870d62385c8c18
Ademilson Santos <ademilsonsantos@biofy.tech>
2026-03-06 11:55:11 -0300

    Rota para listagem de pokemons importados

---
+++ resources/views/pokedex/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app', ['title' => 'Minha Pokédex'])
+@section('content')
+    <div class="w-full">
+        <h1 class="text-2xl font-bold mb-4">Minha Pokédex</h1>
+        <p class="text-gray-600">Aqui estão os Pokémon que você capturou.</p>
+        <div class="w-full mt-10">
+
+        </div>
+    </div>
+@endsection
