61237e3eca1c537d40f4177c6bac815596f501f6
Ademilson <ademilsonmoura3@gmail.com>
2026-03-09 01:28:24 -0300

    Atualizando layout principal para adição de scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,5 +17,6 @@
     <main class="w-full">
         @yield('content')
     </main>
+     @stack('js')
 </body>
 </html>
