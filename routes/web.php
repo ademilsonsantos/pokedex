4d2031b3ca4cf1b0151e45a5461c6d81aeb8deec
Ademilson <ademilsonmoura3@gmail.com>
2026-03-09 01:26:45 -0300

    Atualizando rotas para Api / Remoção de importações e favoritos / Pesquisa dos importados

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\PokedexController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/', [PokedexController::class, 'index'])->name('pokedex.index');
+    Route::get('/{name?}', [PokedexController::class, 'index'])->name('pokemon.index');
+    Route::post('/pokemon/import', [PokedexController::class, 'import'])->name('pokemon.import')->middleware('throttle:10,1');
+    Route::delete('/pokemon/destroy/{id}', [PokedexController::class, 'destroy'])->name('pokemon.destroy');
+    Route::delete('/pokemon/favorite/{id}', [PokedexController::class, 'favorite'])->name('pokemon.favorite');
+    Route::get('/pokemon/show/{id}', [PokedexController::class, 'show'])->name('pokemon.show');
 
-    Route::resource('permission', PermissionController::class, ['except' => 'show']);;
+    Route::resource('permission', PermissionController::class, ['except' => 'show']);
+    Route::resource('user', UserController::class, ['except' => 'show']);
 });
